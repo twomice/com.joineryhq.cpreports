c5ebe2d0337b69c6072b4d969d9530861ac25851
Allen Shaw <allen@JoineryHQ.com>
2020-05-12 08:36:05 -0500

    Added  correct mgd file for Clientroster_Duration.

---
+++ CRM/Cpreports/Form/Report/Clientroster/Duration.mgd.php
@@ -0,0 +1,21 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Cpreports_Form_Report_Clientroster_duration',
+    'entity' => 'ReportTemplate',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'Client Roster: Duration',
+      'description' => '',
+      'class_name' => 'CRM_Cpreports_Form_Report_Clientroster_Duration',
+      'report_url' => 'cpreports/clientroster/duration',
+      'component' => '',
+    ),
+  ),
+);
