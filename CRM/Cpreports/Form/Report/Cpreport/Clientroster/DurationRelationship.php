3698c6cd4efdfa7a7a43d5163de890e9987537da
Allen Shaw <allen@JoineryHQ.com>
2021-08-20 14:51:33 -0500

    Clientroster_DurationRelationship report: add "Relationship days participated" column and related stats.

--- CRM/Cpreports/Form/Report/Cpreport/Clientroster/DurationRelationship.php
+++ CRM/Cpreports/Form/Report/Cpreport/Clientroster/DurationRelationship.php
@@ -12,7 +12,12 @@ class CRM_Cpreports_Form_Report_Cpreport_Clientroster_DurationRelationship exten
    * @inheritdoc
    */
   protected $_useFilterParticipationDates = FALSE;
-  
+
+  /**
+   * @inheritdoc
+   */
+  protected $_useColumnRelationshipDaysParticipated = TRUE;
+
   public function __construct() {
     parent::__construct();
   }
@@ -24,7 +29,7 @@ class CRM_Cpreports_Form_Report_Cpreport_Clientroster_DurationRelationship exten
     // NOTE: Assumes existence of `civicrm_relationship` as `r` in $this->_from.
     $this->_whereClauses[] = "(r.id IS NOT NULL)";
   }
-  
+
   public function statistics(&$rows) {
     $statistics = parent::statistics($rows);
 
@@ -39,33 +44,33 @@ class CRM_Cpreports_Form_Report_Cpreport_Clientroster_DurationRelationship exten
     if ($from) {
       $this->_serviceDateFrom = $from;
     }
-    $this->_addStatisticParticipationEndedDuring($statistics);
+//    $this->_addStatisticParticipationEndedDuring($statistics);
 
     $sqlBase = $this->_getSqlBase();
 
-//    //Total composite duration of all clients (days)
-//    $query = "
-//      select sum({$this->_columns['alias_civicrm_value_participation_6']['fields']['days_participated']['dbAlias']})
-//      from (
-//      select contact_indiv_civireport.id as contact_id
-//      -- sqlbase >>>>
-//      {$sqlBase}
-//      -- <<< sqlbase
-//    ) t
-//    inner join civicrm_value_participation_6 alias_civicrm_value_participation_6_civireport ON alias_civicrm_value_participation_6_civireport.entity_id = t.contact_id
-//    ";
-//    $statistics['counts']['total_days'] = array(
-//      'title' => E::ts("Total of all client duration (days)"),
-//      'value' => CRM_Core_DAO::singleValueQuery($query),
-//      // e.g. CRM_Utils_Type::T_STRING, default seems to be integer
-//      'type' => CRM_Utils_Type::T_INT,
-//    );
+    //Total "days participated (relationships)" values
+    $query = "
+      SELECT
+        SUM (relationship_days_participated)
+      FROM (
+        SELECT {$this->_columns['relationship_days_participated']['fields']['relationship_days_participated']['dbAlias']} as relationship_days_participated
+            -- sqlbase >>>>
+            {$sqlBase}
+            -- <<< sqlbase
+      ) t
+    ";
+    $statistics['counts']['total_days'] = array(
+      'title' => E::ts("Total of all <em>Days Participated (relationships)</em>"),
+      'value' => CRM_Core_DAO::singleValueQuery($query),
+      // e.g. CRM_Utils_Type::T_STRING, default seems to be integer
+      'type' => CRM_Utils_Type::T_INT,
+    );
 
     //Average duration (based on all Team Clients processed)
     $totalRows = $statistics['counts']['rowsFound']['value'] ?? $statistics['counts']['rowCount']['value'];
     $avgValue = ($statistics['counts']['participation_ended_during']['value'] ? ($statistics['counts']['total_days']['value'] / $totalRows) : 'N/A (none ended during this period)');
     $statistics['counts']['average_duration'] = array(
-      'title' => E::ts("Average client duration (days)"),
+      'title' => E::ts("Average <em>Days Participated (relationships)</em>"),
       'value' => $avgValue,
       // e.g. CRM_Utils_Type::T_STRING, default seems to be integer
       'type' => (is_numeric($avgValue) ? CRM_Utils_Type::T_INT : CRM_Utils_Type::T_STRING),
