668e93fa643e187943bf968db7874e594d487737
Allen Shaw <allen@JoineryHQ.com>
2022-04-08 11:32:14 -0500

    F#641: add 'nickname' field and filter to Contact Summary and Contact Detail reports.

--- cpreports.php
+++ cpreports.php
@@ -4,6 +4,46 @@ require_once 'cpreports.civix.php';
 use CRM_Cpreports_ExtensionUtil as E;
 
 /**
+ * Implements hook_civicrm_alterReportVar().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterReportVar
+ */
+function cpreports_civicrm_alterReportVar($varType, &$var, $reportForm) {
+  if ($varType == 'columns') {
+    // Insert nickname filter in certain reports.
+    switch(get_class($reportForm)) {
+      case 'CRM_Report_Form_Contact_Summary':
+      case 'CRM_Report_Form_Contact_Detail':
+        // Shorthand variable for nickname filter/field properties
+        $nickNameFilterFieldProperties = array(
+          'nick_name' => array(
+            'name' => 'nick_name',
+            'title' => ts('Nickname'),
+          ),
+        );
+        // We'll insert this filter after the 'sort_name' filter, so first find out the correct position.
+        $nickNameFilterPosition = array_search('sort_name', array_keys($var['civicrm_contact']['filters'])) + 1;
+        // Insert filter at that position.
+        $var['civicrm_contact']['filters'] =
+          array_slice($var['civicrm_contact']['filters'], 0, $nickNameFilterPosition, true) +
+          $nickNameFilterFieldProperties +
+          array_slice($var['civicrm_contact']['filters'], $nickNameFilterPosition, NULL, true);
+
+        // We'll insert field after the 'sort_name' field , so first find out the correct position.
+        $nickNameFieldPosition = array_search('sort_name', array_keys($var['civicrm_contact']['fields'])) + 1;
+        // Insert field at that position.
+        $var['civicrm_contact']['fields'] =
+          array_slice($var['civicrm_contact']['fields'], 0, $nickNameFieldPosition, true) +
+          $nickNameFilterFieldProperties +
+          array_slice($var['civicrm_contact']['fields'], $nickNameFieldPosition, NULL, true);
+
+        $reportForm->addToDeveloperTab("-- Nickname field and filter added by extension com.joineryhq.cpreports");
+        break;
+    }
+  }
+}
+
+/**
  * Implements hook_civicrm_config().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
